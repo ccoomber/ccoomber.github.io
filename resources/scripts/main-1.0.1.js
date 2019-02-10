19a09a8406e56daa2b22f82e4f43b9d85a9b3f49
Chris Coomber <ccoomber@users.noreply.github.com>
2019-02-10 15:17:09 +0000

    Add noreferrer
    Add rel="noreferrer" to links with target set to _blank

--- resources/scripts/main-1.0.1.js
+++ resources/scripts/main-1.0.1.js
@@ -18,9 +18,9 @@ window.addEventListener("load", function(){
 		elements: {
 			header: '<span class="cc-header z-depth-5">{{header}}</span>&nbsp;',
 			message: '<span id="cookieconsent:desc" class="cc-message">{{message}}</span>',
-			messagelink: '<span id="cookieconsent:desc" class="cc-message">{{message}} <a aria-label="learn more about cookies" tabindex="0" class="cc-link" href="{{href}}" target="_blank">{{link}}</a></span>',
+			messagelink: '<span id="cookieconsent:desc" class="cc-message">{{message}} <a aria-label="learn more about cookies" tabindex="0" class="cc-link" href="{{href}}" target="_blank" rel="noreferrer">Learn more</a></span>',
 			dismiss: '<a aria-label="dismiss cookie message" tabindex="0" class="cc-btn cc-dismiss waves-effect waves-light btn blue-grey darken-2 cookie-dismiss-override">{{dismiss}}</a>',
-			link: '<a aria-label="learn more about cookies" tabindex="0" class="cc-link" href="{{href}}" target="_blank">{{link}}</a>',
+			link: '<a aria-label="learn more about cookies" tabindex="0" class="cc-link" href="{{href}}" target="_blank" rel="noreferrer">Learn more</a>',
 		},
 		window: '<div role="dialog" aria-live="polite" aria-label="cookieconsent" aria-describedby="cookieconsent:desc" class="cc-window z-depth-5 {{classes}}"><!--googleoff: all-->{{children}}<!--googleon: all--></div>'
 	})});
